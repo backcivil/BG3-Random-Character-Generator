982739e990df22e66af0428860f59643c3e94fa4
backcivil <124133350+backcivil@users.noreply.github.com>
2025-08-26 00:01:33 +0900

    set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/BG3-Random-Character-Generator/', // 레포명과 정확히 동일
 })
