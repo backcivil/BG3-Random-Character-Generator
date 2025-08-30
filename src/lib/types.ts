c7458b2318c4c5c88ea116aa9b638b2eace6b112
backcivil <124133350+backcivil@users.noreply.github.com>
2025-08-30 19:41:32 +0900

    Create types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+// 타입 정의
+export type Lang = "ko" | "en";
+
+export const ABILS = ["STR","DEX","CON","INT","WIS","CHA"] as const;
+export type Abil = typeof ABILS[number];
+
+export type Background =
+  | "복사" | "사기꾼" | "범죄자" | "연예인" | "시골 영웅" | "길드 장인"
+  | "귀족" | "이방인" | "현자" | "군인" | "부랑아" | "-";
