e0d94a81c5a3899934813136929fcfbbaf6c1972
backcivil <124133350+backcivil@users.noreply.github.com>
2025-08-30 20:58:22 +0900

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,9 @@
 // 타입 정의
-export type Lang = "ko" | "en";
-
+// src/lib/types.ts
 export const ABILS = ["STR","DEX","CON","INT","WIS","CHA"] as const;
 export type Abil = typeof ABILS[number];
+
+export type Lang = "ko" | "en";
 
 export type Background =
   | "복사" | "사기꾼" | "범죄자" | "연예인" | "시골 영웅" | "길드 장인"
